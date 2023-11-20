8a02ecd03561c9602239d9b0d2ff19cdbdbf056e
JulianAmado <j.amadosena@gmail.com>
2023-11-19 19:58:10 -0500

    Taller 1. Punto 9

--- TallerSecuenciales.php
+++ TallerSecuenciales.php
@@ -1,6 +1,3 @@
-<?php 
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,8 +8,198 @@
     <link rel="shortcut icon" href="Resources/LogoHead.svg" type="image/x-icon">
 </head>
 <body>
+
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item" role="presentation">
+        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Inicio</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="ejercicio-1" data-bs-toggle="tab" data-bs-target="#ejercicio-1-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Ejercicio 1</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="ejercicio-2" data-bs-toggle="tab" data-bs-target="#ejercicio-2-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Ejercicio 2</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="ejercicio-3" data-bs-toggle="tab" data-bs-target="#ejercicio-3-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Ejercicio 3</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="ejercicio-4" data-bs-toggle="tab" data-bs-target="#ejercicio-4-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Ejercicio 4</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="ejercicio-5" data-bs-toggle="tab" data-bs-target="#ejercicio-5-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Ejercicio 5</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="ejercicio-6" data-bs-toggle="tab" data-bs-target="#ejercicio-6-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Ejercicio 6</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="ejercicio-7" data-bs-toggle="tab" data-bs-target="#ejercicio-7-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Ejercicio 7</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="ejercicio-8" data-bs-toggle="tab" data-bs-target="#ejercicio-8-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Ejercicio 8</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="ejercicio-9" data-bs-toggle="tab" data-bs-target="#ejercicio-9-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Ejercicio 9</button>
+    </li>
+    </ul>
+
+    <div class="tab-content" id="myTabContent">
+    <br>
+    <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0"><div class="alert alert-success" role="alert">
+    <h4 class="alert-heading">Objetivo: </h4>
+    <p>Introducirnos a la programación de algoritmos a través del manejo de las estructuras o
+    instrucciones secuenciales, para tal fin aprovecharemos las ventajas que nos brinda la lógica.
+    Para los siguientes puntos (cada uno) desarrollar de manera completa lo siguiente:
+    </p>
+    <p>Análisis del problema respondiéndose las preguntas.
+    <br>a) ¿Qué me piden?
+    <br>b) ¿Qué me dan?
+    <br>c) ¿Cómo lo hago?</p>
+    </div></div>
+
+    <div class="tab-pane fade" id="ejercicio-1-pane" role="tabpanel" aria-labelledby="ejercicio-1" tabindex="0"><div class="alert alert-success" role="alert">
+    <h4 class="alert-heading">Hola bienvenido al desarrollo de programación</h4>
+    </div></div>
+
+    <div class="tab-pane fade" id="ejercicio-2-pane" role="tabpanel" aria-labelledby="ejercicio-2" tabindex="0"><div class="alert alert-success" role="alert">
+    <h4 class="alert-heading">Julian Santiago Amado Guacheta</h4>
+    </div></div>
+
+    <div class="tab-pane fade" id="ejercicio-3-pane" role="tabpanel" aria-labelledby="ejercicio-3" tabindex="0"><div class="alert alert-success" role="alert">
+    <h4 class="alert-heading">Hola, bienvenido Julian Santiago Amado Guacheta</h4>
+    </div></div>
+
+    <div class="tab-pane fade" id="ejercicio-4-pane" role="tabpanel" aria-labelledby="ejercicio-4" tabindex="0"><div class="alert alert-success" role="alert">
+    <form method="POST">
+        <label for="numero">Ingresa un número:</label>
+        <input type="number" name="numero" id="numero">
+        <button type="submit">Enviar</button>
+    </form>
+    <?php
+    $numero = $_POST['numero'];
+    echo "Número que ingresaste: $numero";
+    ?>
+    </div></div>
+
+    <div class="tab-pane fade" id="ejercicio-5-pane" role="tabpanel" aria-labelledby="ejercicio-5" tabindex="0"><div class="alert alert-success" role="alert">
+    <form method="POST">
+        <label for="numeromultiplicado">Ingresa un número:</label>
+        <input type="number" name="numeromultiplicado" id="numeromultiplicado">
+        <button type="submit">Enviar</button>
+    </form>
+    <?php
+    $numeromultiplicado = $_POST['numeromultiplicado'];
+    $diez = 10;
+    $multiplicado = $numeromultiplicado * $diez;
+    echo "Número que ingresaste: $multiplicado";
+    ?>
+    </div></div>
     
+    <div class="tab-pane fade" id="ejercicio-6-pane" role="tabpanel" aria-labelledby="ejercicio-6" tabindex="0"><div class="alert alert-success" role="alert">
+    <form method="POST">
+        <label for="numero1">Ingresa un número 1:</label>
+        <input type="number" name="numero1" id="numero1">
+        <br>
+        <br>
+        <label for="numero2">Ingresa un número 2:</label>
+        <input type="number" name="numero2" id="numero2">
+        <br>
+        <br>
+        <button type="submit">Enviar</button>
+        <br>
+        <br>
+    </form>
+    <?php
+    $numero1 = $_POST['numero1'];
+    echo "Número 1 que ingresaste: $numero1 <br><br>";
+    $numero2 = $_POST['numero2'];
+    echo "Número 2 que ingresaste: $numero2 <br>";
+    ?>
+    </div></div>
     
+    <div class="tab-pane fade" id="ejercicio-7-pane" role="tabpanel" aria-labelledby="ejercicio-7" tabindex="0"><div class="alert alert-success" role="alert">
+    <form method="POST">
+        <label for="numerosuma1">Ingresa un número 1:</label>
+        <input type="number" name="numerosuma1" id="numerosuma1">
+        <br>
+        <br>
+        <label for="numerosuma2">Ingresa un número 2:</label>
+        <input type="number" name="numerosuma2" id="numerosuma2">
+        <br>
+        <br>
+        <button type="submit">Enviar</button>
+    </form>
+    <?php
+    $numerosuma1 = $_POST['numerosuma1'];
+    $numerosuma2 = $_POST['numerosuma2'];
+    $suma = $numerosuma1 + $numerosuma2;
+    ?>
+    <br>
+    <?php
+    echo "Resultado de la suma: $suma";
+    ?>
+    </div></div>
+
+    <div class="tab-pane fade" id="ejercicio-8-pane" role="tabpanel" aria-labelledby="ejercicio-8" tabindex="0"><div class="alert alert-success" role="alert">
+    <form method="POST">
+        <label for="cantidad">Ingresa una cantidad en pesos colombianos:</label>
+        <input type="number" name="cantidad" id="cantidad">
+        <br>
+        <br>
+        <button type="submit">Enviar</button>
+    </form>
+    <?php
+    $cantidad = $_POST['cantidad'];
+    $dolar = 2000;
+    $euro = 3000;
+    $totalDolar = $cantidad / $dolar;
+    $totalEuro = $cantidad / $euro;
+    ?>
+    <br>
+    <?php
+    echo "Valor equivalente en dolares: $totalDolar";
+    ?>
+    <br>
+    <?php
+    echo "Valor equivalente en euros: $totalEuro";
+    ?>
+    </div></div>
+
+    <div class="tab-pane fade" id="ejercicio-9-pane" role="tabpanel" aria-labelledby="ejercicio-9" tabindex="0"><div class="alert alert-success" role="alert">
+    <form method="POST">
+        <label for="cantidadDolares">Valor del dólar en pesos:</label>
+        <input type="number" name="cantidadDolares" id="cantidadDolares">
+        <br>
+        <br>
+        <label for="cantidadEuros">Valor del euro en pesos:</label>
+        <input type="number" name="cantidadEuros" id="cantidadEuros">
+        <br>
+        <br>
+        <label for="cantidadPC">Ingresa una cantidad en pesos colombianos:</label>
+        <input type="number" name="cantidadPC" id="cantidadPC">
+        <br>
+        <br>
+        <button type="submit">Enviar</button>
+    </form>
+    <?php
+    $cantidadDolares = $_POST['cantidadDolares'];
+    $cantidadEuros = $_POST['cantidadEuros'];
+    $cantidadPC = $_POST['cantidadPC'];
+    $DolarColombia = $cantidadPC / $cantidadDolares;
+    $EuroColombia = $cantidad / $cantidadEuros;
+    ?>
+    <br>
+    <?php
+    echo "Valor equivalente de dolares a colombianos: $DolarColombia";
+    ?>
+    <br>
+    <?php
+    echo "Valor equivalente de euros a colombianos: $EuroColombia";
+    ?>
+    </div></div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
